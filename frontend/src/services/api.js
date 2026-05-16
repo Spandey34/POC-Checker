cda9b6f24cc0ce2353a98a549b324864ac43131b
Spandey34 <shubhamkumarpandey1844@gmail.com>
2026-05-17 00:56:30 +0530

    token-fix-to-resolve-errors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,36 @@
 import axios from 'axios';
+
 import { API_URL } from '../config/constants';
 
-const api = axios.create({ baseURL: API_URL });
+const api = axios.create({
+  baseURL: API_URL,
+});
 
-// Attach Clerk token to every request
-export const setAuthToken = (token) => {
-  if (token) {
-    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  } else {
-    delete api.defaults.headers.common['Authorization'];
-  }
+let getTokenFn = null;
+
+export const injectTokenGetter = (
+  fn
+) => {
+  getTokenFn = fn;
 };
+
+api.interceptors.request.use(
+  async (config) => {
+    if (getTokenFn) {
+      const token =
+        await getTokenFn();
+
+      if (token) {
+        config.headers.Authorization =
+          `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+
+  (error) =>
+    Promise.reject(error)
+);
 
 export default api;
