2d48ce925148461e38792fc45b06619238e516c6
Spandey34 <shubhamkumarpandey1844@gmail.com>
2026-05-16 19:48:43 +0530

    removed-strictmode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,7 +15,6 @@ if (!PUBLISHABLE_KEY)
 ReactDOM.createRoot(
   document.getElementById('root')
 ).render(
-  <React.StrictMode>
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
       signInFallbackRedirectUrl="/dashboard"
@@ -23,5 +22,4 @@ ReactDOM.createRoot(
     >
       <App />
     </ClerkProvider>
-  </React.StrictMode>
 );
